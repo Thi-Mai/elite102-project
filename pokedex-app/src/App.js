903453037f28d25d6ecdb784c029cf8993df2b63
thi-mai <s1671241@online.houstonisd.org>
2022-05-17 20:18:53 -0500

    test

--- pokedex-app/src/App.js
+++ pokedex-app/src/App.js
@@ -1,3 +1,70 @@
+/*
+import './App.css';
+import Pokedex from 'pokedex-promise-v2';
+
+import React from 'react';
+
+
+import { ReactDOM } from 'react';
+
+
+const P = new Pokedex();
+
+P.getPokemonByName("butterfree")
+.then((response) => {
+  console.log(response);
+})
+.catch((error) => {
+  console.log('There was an ERROR: ', error);
+});
+
+
+
+function App() {
+  return (
+    <div className="App-header">
+      Hello World
+    </div>
+  );
+
+}
+
+
+
+export default App;
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+
+
+class Pokemon extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        Pokemon Info
+      </div>
+
+    );
+  }
+}
+
+export default Pokemon;
+
+class HelloMessage extends React.Component {
+  render() {
+    return <div>Hello {this.props.name}</div>;
+  }
+}
+
+root.render(<HelloMessage name="Taylor" />);
+
+export default HelloMessage;
+*/
+
+
+
 import './App.css';
 
 function App() {
