0db56ab8cbda68d7ab4c1506b0cf0b06407d4d95
thi-mai <s1671241@online.houstonisd.org>
2022-05-17 23:11:11 -0500

    added user input spot

--- pokedex-app/src/App.js
+++ pokedex-app/src/App.js
@@ -1,6 +1,54 @@
 import './App.css';
 import React from 'react';
 
+/*
+import Pokedex from 'pokedex-promise-v2';
+const P = new Pokedex();
+
+
+P.getPokemonByName(['eevee', 'ditto']) // with Promise
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log('There was an ERROR: ', error);
+  });
+*/
+
+export default function App() {
+  // onChnage Handler funtion
+  // with event parameter
+  const getInputValue = (event) => {
+    // show the user input value to console
+    //const userValue = event.target.value;
+
+    //console.log(userValue);
+  };
+
+
+  return (
+    <div>
+
+      <form className='App'>
+        <label>
+            Pokemon search:
+        <input type="text"/>
+       </label>
+      <input type="submit" value="Submit" />
+      </form>
+
+      <div className="App-header">
+        Pokemon Info
+      </div>
+
+    </div>
+  );
+
+
+}
+
+
+
 
 class Pokemon extends React.Component {
   render() {
@@ -13,4 +61,4 @@ class Pokemon extends React.Component {
   }
 }
 
-export default Pokemon;
+//export default Pokemon;
