ac1da98050b01ab59cf5ac314023a574d55aeb0b
thi-mai <s1671241@online.houstonisd.org>
2022-05-18 23:22:57 -0500

    display user input

--- pokedex-app/src/App.js
+++ pokedex-app/src/App.js
@@ -15,6 +15,7 @@ P.getPokemonByName(['eevee', 'ditto']) // with Promise
   });
 */
 
+/*
 export default function App() {
 
   return (
@@ -29,26 +30,105 @@ export default function App() {
       </form>
 
       <div className="App-header">
-        Pokemon Info
+        <h1>Pokemon Info</h1>
+        <h2>value="hello"</h2>
+        
       </div>
 
     </div>
   );
 
 }
-
+*/
 
 /*
 class Pokemon extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A pokemon was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div className="App-header">
-        Pokemon Info
+      <div>
+  
+        <form className='App-input' onSubmit={this.handleSubmit}>
+          <label>
+              Pokemon search:
+          <input type="text" value={this.state.value} onChange={this.handleChange}/>
+         </label>
+        <input type="submit" value="Submit" />
+        </form>
+  
+        <div className="App">
+          <h1>Pokemon Info</h1>
+          <h2>second line</h2>
+          
+        </div>
+  
       </div>
-
     );
   }
+
 }
 
 export default Pokemon;
 */
+
+class Pokemon extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A pokemon was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+
+    var pokemonName = this.state.value;
+
+    return (
+      <div>
+  
+        <form className='App-input' onSubmit={this.handleSubmit}>
+          <label>
+              Pokemon search:
+          <input type="text" value={this.state.value} onChange={this.handleChange}/>
+         </label>
+        <input type="submit" value="Submit" />
+        </form>
+  
+        <div className="App">
+          <h1>Pokemon: {pokemonName}</h1>
+          <h2> Info </h2>
+        </div>
+  
+      </div>
+    );
+  }
+
+}
+
+export default Pokemon;
