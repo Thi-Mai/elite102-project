fd596cb64c00996b79504bd873f1b411e3f76602
thi-mai <s1671241@online.houstonisd.org>
2022-05-21 15:39:22 -0500

    added fetch

--- pokedex-app/src/App.js
+++ pokedex-app/src/App.js
@@ -1,103 +1,28 @@
 import './App.css';
 import React from 'react';
 
-/*
-import Pokedex from 'pokedex-promise-v2';
-const P = new Pokedex();
 
+const url = 'https://pokeapi.co/api/v2/';
 
-P.getPokemonByName(['eevee', 'ditto']) // with Promise
-  .then((response) => {
+class Pokemon extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  async handleChange(event) {
+    this.setState({value: event.target.value});
+
+    var response = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu');
+
+    var data = await response.json()
+
+    this.setState({...this.state, info: JSON.stringify(data) })
     console.log(response);
-  })
-  .catch((error) => {
-    console.log('There was an ERROR: ', error);
-  });
-*/
 
-/*
-export default function App() {
-
-  return (
-    <div>
-
-      <form className='App'>
-        <label>
-            Pokemon search:
-        <input type="text"/>
-       </label>
-      <input type="submit" value="Submit" />
-      </form>
-
-      <div className="App-header">
-        <h1>Pokemon Info</h1>
-        <h2>value="hello"</h2>
-        
-      </div>
-
-    </div>
-  );
-
-}
-*/
-
-/*
-class Pokemon extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A pokemon was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <div>
-  
-        <form className='App-input' onSubmit={this.handleSubmit}>
-          <label>
-              Pokemon search:
-          <input type="text" value={this.state.value} onChange={this.handleChange}/>
-         </label>
-        <input type="submit" value="Submit" />
-        </form>
-  
-        <div className="App">
-          <h1>Pokemon Info</h1>
-          <h2>second line</h2>
-          
-        </div>
-  
-      </div>
-    );
-  }
-
-}
-
-export default Pokemon;
-*/
-
-class Pokemon extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
   }
 
   handleSubmit(event) {
@@ -122,7 +47,10 @@ class Pokemon extends React.Component {
   
         <div className="App">
           <h1>Pokemon: {pokemonName}</h1>
-          <h2> Info </h2>
+          <p> Info: </p>
+          <p>
+            {this.state.info}
+          </p>
         </div>
   
       </div>
